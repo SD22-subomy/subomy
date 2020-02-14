26d7ff0e5621f3fd26427998ddbd730e1452c434
naka <shadowkirby.nagi@gmail.com>
2020-02-14 16:21:57 +0900

    func

--- function/func.php
+++ function/func.php
@@ -26,4 +26,25 @@ function h($s)
   return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
 }
 
+
+/**
+ * 渡された文字列がメールアドレスかをチェックする関数
+ *
+ * @param string $address チェックしたい文字列
+ * @return bool メールアドレスの場合はtrue 違う場合はfalse
+ */
+function isEmail(string $address)
+{
+  if (filter_var($address, FILTER_VALIDATE_EMAIL)) {
+    return true;
+  }
+  if (strpos($address, '@docomo.ne.jp') !== false || strpos($address, '@ezweb.ne.jp') !== false) {
+    $pattern = '/^([a-zA-Z])+([a-zA-Z0-9\._-])*@(docomo\.ne\.jp|ezweb\.ne\.jp)$/';
+    if (preg_match($pattern, $address) === 1) {
+      return true;
+    }
+  }
+  return false;
+}
+
 ?>
