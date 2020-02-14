4014ae50dcc83729f228887603d2960046c27b28
naka <shadowkirby.nagi@gmail.com>
2020-02-14 15:53:56 +0900

    function

--- function/func.php
+++ function/func.php
@@ -9,7 +9,7 @@
 
 
 
-//定数読み込み
+//読み込み
 require './../config.php';
 
 
@@ -25,40 +25,5 @@ function h($s)
 {
   return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
 }
-
-
-
-
-/**
- * ＜店舗＞ログイン時のIDパスワードが一致するか調べる関数
- *
- * @param string $id 入力ID
- * @param string $pass 入力パスワード
- * @return bool ログインできる場合はtrue できない場合はfalse
- */
-
-function shop_login($id,$pass){
-
-    $cn = mysqli_connect(HOST,DB_USER,DB_PASS,DB_NAME);
-    mysqli_set_charset($cn,'utf8');
-    $sql = "SELECT pass from shop WHERE login_id = '".h($id)."';";
-    $result = mysqli_query($cn,$sql);
-    $row = mysqli_fetch_assoc($result); 
-    mysqli_close($cn);
-
-    // if(is_null($row['pass'])){
-    //     $msg = 'ログインIDが正しくありません';
-    // }else if($row['pass'] == h($pass)){
-    //     $msg = 'ログイン完了';
-    // }else{
-    //     $msg = 'パスワードが正しくありません';
-    // }
-
-    if($row['pass'] == h($pass)){
-        return true;
-    }
-    return false;
-}
-
 
 ?>
