4469524e48dab29a9a9ef73c065863cc9ce80a2e
naka <shadowkirby.nagi@gmail.com>
2020-02-17 14:12:10 +0900

    edit file

--- user/controller/sign_up.php
+++ user/controller/sign_up.php
@@ -12,31 +12,33 @@ require_once './../../function/user_func.php';
 
 session_start();
 
-require_once './../user_tpl/sign_up.html';
 
 if(isset($_POST['send'])){
 
-	$name = $_POST['name'];
-	$tel = $_POST['tel'];
-	$gender = $_POST['gender'];
-	$mail = $_POST['mail'];
-	$pass = $_POST['pass'];
+	// $name = $_POST['name'];
+	// $tel = $_POST['tel'];
+	// $gender = $_POST['gender'];
+	// $mail = $_POST['mail'];
+	// $pass = $_POST['pass'];
 
-	if(user_regist_check($tel,$mail)){
+	// if(user_regist_check($tel,$mail)){
 
-		$_SESSON['name'] = $name;
-		$_SESSION['tel'] = $tel;
-		$_SESSION['gender'] = $gender;
-		$_SESSION['mail'] = $mail;
-		$_SESSION['pass'] = $pass;
+	// 	$_SESSON['name'] = $name;
+	// 	$_SESSION['tel'] = $tel;
+	// 	$_SESSION['gender'] = $gender;
+	// 	$_SESSION['mail'] = $mail;
+	// 	$_SESSION['pass'] = $pass;
 
-		require_once './../user_tpl/sign_up_check.html';
-		exit;
-	}else{
+	// 	require_once './../user_tpl/sign_up_check.html';
+	// 	exit;
+	// }else{
 
-		require_once './../user_tpl/sign_up.html';
-		exit;
-	}
+	// 	require_once './../user_tpl/sign_up.html';
+	// 	exit;
+	// }
+
+	require_once './../user_tpl/sign_up_check.html';
+	exit;
 }
 
 if(isset($_POST['back'])){
@@ -47,14 +49,13 @@ if(isset($_POST['back'])){
 
 if(isset($_POST['regist'])){
 
-	require_once './../user_tpl/sign_up_over';
+	session_destroy();
+	require_once './../user_tpl/sign_up_over.html';
 	exit;
 }
 
-
-require_once './../index.php'
-
 session_destroy();
+require_once './../user_tpl/sign_up.html';
 
 
 ?>
