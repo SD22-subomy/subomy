7fdc21ca86165e4d62140c35bf8202be013a8aab
naka <shadowkirby.nagi@gmail.com>
2020-02-20 01:52:14 +0900

    edit tpl

--- user/user_tpl/shop.php
+++ user/user_tpl/shop.php
@@ -15,7 +15,28 @@
 
     <div id="content">
         <div class="center">
-            PIZZA SALVATORE CUOMO&GRILL
+            <ul>
+                <li>PIZZA SALVATORE CUOMO&GRILL</li>
+                <li>050-1234-5678</li>
+                <li>大阪府北区</li>
+            </ul>
+            <table border="1">
+                <tr>
+                    <td>コース名</td>
+                    <td>料金</td>
+                    <td>備考</td>
+                </tr>
+                <tr>
+                    <td>飲み放題コース</td>
+                    <td>5000</td>
+                    <td>土日祝は+500円</td>
+                </tr>
+                <tr>
+                    <td>スタンダードコース</td>
+                    <td>4000</td>
+                    <td>ピザ食べ放題</td>
+                </tr>
+            </table>
         </div>
     </div>
 
