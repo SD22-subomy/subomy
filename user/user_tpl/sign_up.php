513cd792e3c30b2976eba4f92fb040ad02bd7206
fukuda0609 <ana69321@gmail.com>
2020-02-22 21:53:19 +0900

    デザイン

--- user/user_tpl/sign_up.php
+++ user/user_tpl/sign_up.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>新規会員確認画面</title>
-<link rel="stylesheet" href="#">
+<link rel="stylesheet" href="../user_css/sign_up.css">
 </head>
 <body>
 <div id="wrapper">
@@ -18,30 +18,30 @@
             <form action="" method="POST">
                 <table>
                     <tr>
-                        <td>氏名:</td>
+                        <th>氏名</th>
                         <td><input type="text" name="name" value="<?php echo $name;?>"></td>
                     </tr>
                     <tr>
-                        <td>電話番号:</td>
+                        <th>電話番号</th>
                         <td><input type="text" name="tel" value="<?php echo $tel;?>"></td>
                     </tr>
                     <tr>
-                        <td>性別:</td>
+                        <th>性別</th>
                         <td>
-                            <input type="radio" name="gender" value="0" <?php if(empty($gender) || $gender == 0){ echo 'checked';}?>>男
-                            <input type="radio" name="gender" value="1" <?php if($gender == 1){ echo 'checked';}?>>女
+                            <label><input type="radio" name="gender" value="0" <?php if(empty($gender) || $gender == 0){ echo 'checked';}?>>男</label>
+                            <label><input type="radio" name="gender" value="1" <?php if($gender == 1){ echo 'checked';}?>>女</label>
                         </td>
                     </tr>
                     <tr>
-                        <td>メールアドレス:</td>
+                        <th>メールアドレス</th>
                         <td><input type="text" name="mail" value="<?php echo $mail;?>"></td>
                     </tr>
                     <tr>
-                        <td>パスワード:</td>
+                        <th>パスワード</th>
                         <td><input type="text" name="pass"></td>
                     </tr>
                     <tr>
-                        <td colspan="2"><input type="submit" name='send' value="確認"></td>
+                        <td colspan="2"><input type="submit" class="btn" name='send' value="確認"></td>
                     </tr>
                 </table>
             </form>
