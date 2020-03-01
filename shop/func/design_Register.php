ae7e22ac71cc5bba909439d66db6e091c8779f2a
fukuda0609 <ana69321@gmail.com>
2020-03-01 21:54:34 +0900

    デザイン

---
+++ shop/func/design_Register.php
@@ -0,0 +1,22 @@
+<?php
+/////////////////////////////////////////
+require 'func.php';//関数呼び出し
+require '../../config.php';
+/////////////////////////////////////////
+session_start();
+$id = $_SESSION['shop_id'];
+
+$color = $_SESSION['color_id'];
+$design = $_SESSION['design_id'];
+
+//データベース接続(shop_design)
+$cn = mysqli_connect(HOST,DB_USER,DB_PASS,DB_NAME);
+mysqli_set_charset($cn,'utf8');
+
+$col = ['color_id','design_id'];
+$what = [$color,$design];
+$sql = shopDesign('shop_design',$col,$what,$id);
+update($cn,$sql);
+
+require '../shop_tpl/design_Register.php';
+?>
